b307b24bf8a3ee55b896a1cbad87838cf99e08c3
xionghaoo <xionghao930316@gmail.com>
2022-06-26 17:38:53 +0800

    按文件类型加载

--- src/view/Content.vue
+++ src/view/Content.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <img :src="require(`@/assets/${content_uri}`)" width="100%" height="100%">
+    <img v-if="file_type === 0" :src="require(`@/assets/${img_uri}`)" width="100%" height="100%">
+    <video v-else-if="file_type === 1" id="content_video" width="100%" height="100%" autoplay loop>
+      <source :src="require(`@/assets/${video_uri}`)" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
   </div>
 </template>
 
 <script>
 import IPC from "@/utils/ipc";
-// import img_url from "@/assets/huawei/huawei1.jpg"
-
 
 export default {
   name: "Content",
@@ -17,7 +19,9 @@ export default {
   data() {
     return {
       ipc: null,
-      content_uri: ''
+      img_uri: 'logo.png',
+      video_uri: '',
+      file_type: 0
     }
   },
   created() {
@@ -28,7 +32,27 @@ export default {
         let s = screens[i]
         // 屏幕匹配
         if (s.index === _this.index + 1) {
-          this.content_uri = s.item_uri.replace('/static/', '')
+          this.file_type = s.file_type
+          let file_uri = s.item_uri.replace('/static/', '')
+          switch (s.file_type) {
+            case 0:
+              // 图片
+              this.img_uri = file_uri
+              break;
+            case 1: {
+              // 视频
+              this.video_uri = file_uri
+              let video = document.getElementById("content_video");
+              // console.log("play mp4: " + this.content_url)
+              let source = document.createElement('source');
+              video.pause();
+              source.setAttribute('src', "require(`@/assets/${video_uri}`)");
+              source.setAttribute('type', 'video/mp4');
+              video.load();
+              video.play();
+              break;
+            }
+          }
         }
       }
     })
