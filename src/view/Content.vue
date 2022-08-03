3f2fcd45c8230a1e860a09d5a8118b9417346d44
xionghaoo <xionghao930316@gmail.com>
2022-08-03 16:03:25 +0800

    测试

--- src/view/Content.vue
+++ src/view/Content.vue
@@ -12,7 +12,7 @@
         </div>
       </transition>
     </div>
-    <video v-else-if="file_type === 1" id="content_video" width="100%" height="100%" autoplay loop muted>
+    <video v-else-if="file_type === 1" id="content_video" width="100%" height="100%" autoplay loop>
       <source :src="video_uri" type="video/mp4">
       Your browser does not support the video tag.
     </video>
