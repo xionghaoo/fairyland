c5b2cc9f9479b14b99f99a9b73b92fbfb6e81c82
xionghaoo <xionghao930316@gmail.com>
2022-12-23 00:59:49 +0800

    发版

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports  = {
     publicPath: process.env.NODE_ENV  ===  'production'  ?  './'  :  '/'
 }
 
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = 'production'
 
 // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS']=true
