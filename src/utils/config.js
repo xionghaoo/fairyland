01438af61c98d981d734576e3e647cfb117ac515
jeff.zhong <edwardloveyou@gmail.com>
2023-02-28 19:35:51 +0800

    update config

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,36 +3,31 @@
  */
 import { RequestHttpDev, RequestHttpPro } from '../../configBase';
 
-const Config = (() => {
-	console.log('env: ', process.env.NODE_ENV);
-	const baseUrl = process.env.NODE_ENV === 'production' ? RequestHttpPro : RequestHttpDev;
-	// baseUrl = 'http://192.168.8.130:5002' // TODO 测试
-	const api = baseUrl + '/api';
-	console.log('host: ' + api);
-	return {
-		// ajax_http请求url
-		host: api,
-		resourceHost: baseUrl + '/img',
-		api: {
-			/*-- 基础信息 --*/
-			login: api + '/fairyland/login',
-			versionUpdate: api + '/fairyland/update',
-			cardList: api + '/fairyland/card_list',
-			record: baseUrl + '/phantom/sections/recognize_record',
-		},
-		imageWidth: 1280,
-		imageHeight: 960,
-		recognizeInterval: 3000,
-		recognizeApi: 'ws://120.76.175.224:9001',
-		ossHost: 'https://roboland-deliv.ubtrobot.com/',
-		recognizeThreshold: 600,
-		unConfigThreshold: 5,
-		commandThreshold: 3,
-		deviceId: 'wuhan01',
-		api_text_recognize: 'https://rvi.ubtrobot.com/aipocket_new/ocr/zh_en/recognition',
-		// deviceId: "sz001"
-		// recognizeApi: "ws://119.23.154.148:9006"
-	};
-})();
+console.log('env: ', process.env.NODE_ENV);
+const baseUrl = process.env.NODE_ENV === 'production' ? RequestHttpPro : RequestHttpDev;
+// baseUrl = 'http://192.168.8.130:5002' // TODO 测试
+const api = baseUrl + '/api';
+console.log('host: ' + api);
 
-export default Config;
+export default {
+	host: api,
+	resourceHost: baseUrl + '/img',
+	api: {
+		login: api + '/fairyland/login',
+		versionUpdate: api + '/fairyland/update',
+		cardList: api + '/fairyland/card_list',
+		record: baseUrl + '/phantom/sections/recognize_record',
+	},
+	imageWidth: 1280,
+	imageHeight: 960,
+	recognizeInterval: 3000,
+	recognizeApi: 'ws://120.76.175.224:9001',
+	ossHost: 'https://roboland-deliv.ubtrobot.com/',
+	recognizeThreshold: 600,
+	unConfigThreshold: 5,
+	commandThreshold: 3,
+	deviceId: 'wuhan01',
+	api_text_recognize: 'https://rvi.ubtrobot.com/aipocket_new/ocr/zh_en/recognition',
+	// deviceId: "sz001"
+	// recognizeApi: "ws://119.23.154.148:9006"
+};
